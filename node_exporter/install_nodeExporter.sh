3e8dd5436bc5f183bf37b35661c30964bab0e584
Rodrigo Freitas <rodrigof@way2.com.br>
2023-12-07 15:03:10 -0300

    Alterar script node exporter

--- node_exporter/install_nodeExporter.sh
+++ node_exporter/install_nodeExporter.sh
@@ -1,8 +1,31 @@
 # Go to https://github.com/prometheus/node_exporter/releases to get the latest version number.
-node_exporter_version="1.7.0"
-node_exporter_release="linux-amd64"
-# install depedency
-sudo apt install apache2-utils -yq
+echo "Script para instalar node exporter em vm linux"
+echo "--------------------------------------------------------------------------"
+echo "Verificar se dependências foram instaladas com sucesso"
+list=`apt -qq list jq apache2-utils |grep installed`
+if [ -z "${list}" ]
+  then
+    echo "Instalar dependências para o node exporter"
+    sudo apt update -yq && sudo apt install apache2-utils jq -yq
+    clear
+  else
+    echo "Dependências já estão instaladas"
+    sleep 2
+    clear
+fi
+echo "--------------------------------------------------------------------------"
+echo "Listar as ultimas 5 versões disponíveis de node-exporter"
+curl -sL https://api.github.com/repos/prometheus/node_exporter/tags |jq -r ".[].name" |head -n 5|sed 's/v//g'
+export node_exporter_version="1.7.0"
+export node_exporter_release="linux-amd64"
+echo $node_exporter_version
+read -p "insira versão desejada ou pressione enter para utilizar versão default 1.7.0: " nodeVersion
+if [ ! -z "${nodeVersion}" ]
+  then
+    export node_exporter_version=$nodeVersion
+    echo "Será instalado o node exporter na versão $node_exporter_version"
+    sleep 2
+fi
 # Download and install node_exporter
 cd /tmp
 wget https://github.com/prometheus/node_exporter/releases/download/v${node_exporter_version}/node_exporter-${node_exporter_version}.${node_exporter_release}.tar.gz
@@ -55,4 +78,24 @@ echo "Clear password to keep for Prometheus Server: ${password}" > ~/password_ac
 # Start the node_exporter daemon and check its status
 sudo systemctl stop node_exporter
 sudo systemctl start node_exporter
-sudo systemctl status node_exporter
+activeNode=`ss -ltn |grep 9100 |cut -d ':' -f 2 |cut -d ' ' -f 1`
+if [ -z "${activeNode}" ]
+  then
+    echo "NODE EXPORTER ESTÁ RODANDO NA PORTA 9100"
+    echo "sudo systemctl restart node_exporter"
+    read -p "Node exporter reiniciado pressione enter para continuar"
+  else
+    echo "NODE EXPORTER ESTÁ ATIVO E RODANDO NA PORTA 9100"
+    sudo systemctl status node_exporter
+    read -p "Pressione enter para continuar"
+fi
+clear
+hostIP=`ip a s eth0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2`
+pubIP=`curl -s ifconfig.me`
+echo "--------------------------------------------------------------------------"
+echo "para ter mais informações sobre a versão instalada acesse"
+echo "https://github.com/prometheus/node_exporter/releases"
+echo "--------------------------------------------------------------------------"
+echo "Para validar acesse local http://$hostIP:9100/metrics"
+echo "utilizando o usuário prometheus com a senha criada no usuário root"
+echo "Para validar o acesso publico acesse http://$pubIP:9100/metrics com o mesmo usuário e senha"
